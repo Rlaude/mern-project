207d5e20e1da1f03ba679664a88828f24ad791ad
Ria Laude <riarizza_laude@yahoo.com>
2023-03-31 03:48:24 -0700

    Added logo with link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
+import creativeNarrativeLogo from '../assets/creativeNarrativeLogo.PNG'
 
 const Navbar = () => {
-
+    const logoStyle = {
+        height: "100px",
+        width: "100px",
+        display: "flex"
+    }
+    
     return (
         <header>
             <div className="container">
                 <Link to="/">
+                <img src={creativeNarrativeLogo} alt="logo"style={logoStyle}/>
+                </Link>
+                <Link to="/">
+                
                     <h1>Creative Narratives</h1>
                 </Link>
                 <Link to="/about">
